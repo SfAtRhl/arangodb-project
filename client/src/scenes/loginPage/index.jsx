b6e924fcebfcd247a02f846648ab2fcb4c360de7
SfAtRhl <soufyane.aitrehail@gmail.com>
2024-02-16 01:06:52 +0100

    init

---
+++ client/src/scenes/loginPage/index.jsx
@@ -0,0 +1,59 @@
+import { DarkMode, LightMode } from "@mui/icons-material";
+import {
+  Box,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import Form from "./Form";
+import FlexBetween from "components/FlexBetween";
+
+const LoginPage = () => {
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const dispatch = useDispatch();
+
+  const dark = theme.palette.neutral.dark;
+
+  return (
+    <Box>
+      <Box
+        width="100%"
+        backgroundColor={theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign="center"
+      >
+        <FlexBetween gap="1.75rem">
+          <Typography fontWeight="bold" fontSize="32px" color="primary">
+            Sociopedia
+          </Typography>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+        </FlexBetween>
+      </Box>
+
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroundColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socipedia, the Social Media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
